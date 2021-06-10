bcfa013205a41b0c4766f6f3743e03f27c86e6a1
mahan-noor <ismahanabey@gmail.com>
2021-06-10 20:33:55 +0300

    import model classes to views and thr forms class too

--- artapp/views.py
+++ artapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,redirect, get_object_or_404, Http404
-
+from django.contrib.auth.decorators import login_required
+from . models import Post, Profile, Comments
+from . forms import PostComments, PostImagesForm,PostProfile, UpdateUserProfileForm, NewsLetterForm
+from django.contrib.auth.models import User
 # Create your views here.
 
 def home(request):
