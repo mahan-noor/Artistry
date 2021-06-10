ef1e238b107f67b08b2d2af602a65d31b286e8f3
mahan-noor <ismahanabey@gmail.com>
2021-06-10 08:30:07 +0300

    add apps in installed  apps also postgresql in settings

--- artistic/settings.py
+++ artistic/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'artapp',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -75,10 +77,13 @@ WSGI_APPLICATION = 'artistic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'art',
+        'USER': 'lenovo',
+    'PASSWORD':'1234',
     }
 }
+
 
 
 # Password validation
