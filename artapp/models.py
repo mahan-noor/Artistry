e6da35e90c2e577a5d7b4ad842a53cd67ae05e2f
mahan-noor <ismahanabey@gmail.com>
2021-06-10 20:14:55 +0300

    add class profile and method to create,save and get users profile

--- artapp/models.py
+++ artapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import datetime as dt
 
 # Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    profile_picture = models.ImageField(blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+        
+        
+    @classmethod  
+    def get_by_id(cls, id):
+        profile = Profile.objects.get(user = id)
+        return profile
+        
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
+    
