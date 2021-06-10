a43406a9511675e34e35cb7a5527d27ce1fceedf
mahan-noor <ismahanabey@gmail.com>
2021-06-10 20:20:16 +0300

    create post class that allows user to post their master piece and add method to save,delete and get post

--- artapp/models.py
+++ artapp/models.py
@@ -34,4 +34,31 @@ class Profile(models.Model):
     def filter_by_id(cls, id):
         profile = Profile.objects.filter(user = id).first()
         return profile
+
+
     
+class Post(models.Model):
+    title = models.CharField(blank=False, max_length=500)
+    instagram = models.CharField(max_length=100, blank=True)
+    description =models.TextField(max_length=500, blank=False)
+    photo = models.ImageField(blank=False, upload_to='images/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateField(auto_now_add=True, blank=True)
+    
+    def __str__(self):
+        return f'{self.title}'
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        posts = Post.objects.all()
+        return posts
+
+    def save_post(self):
+        self.save()
