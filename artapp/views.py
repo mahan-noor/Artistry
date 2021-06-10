8ea6cad2326476b6d692d98955dce86c8af05926
mahan-noor <ismahanabey@gmail.com>
2021-06-11 00:05:07 +0300

    add about and photo function to get photo and about the page and add their html file

--- artapp/views.py
+++ artapp/views.py
@@ -9,3 +9,17 @@ def home(request):
     title = 'Artistic  || World'
     
     return render(request, 'landing/index.html',{'title': title})
+
+def about(request):
+    title = 'Artistic  || World'
+    form = NewsLetterForm()
+    
+    return render(request, 'about.html',{'title': title, 'form': form})
+    
+
+@login_required(login_url='login')
+def photos(request):
+    title = 'Artistic  || World'
+    posts = Post.objects.all()
+    form = NewsLetterForm()
+    return render(request,'photo.html',{'posts':posts , 'title':title, 'form':form})
