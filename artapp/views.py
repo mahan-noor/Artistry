3b22e229971b741d12ff7a95000efb0711d968fd
mahan-noor <ismahanabey@gmail.com>
2021-06-10 08:15:33 +0300

    add a function that render to the landing page

--- artapp/views.py
+++ artapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404, Http404
 
 # Create your views here.
+
+def home(request):
+    title = 'Artistic  || World'
+    
+    return render(request, 'landing/index.html',{'title': title})
